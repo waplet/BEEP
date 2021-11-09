d736a2c67c743fb277e44a8fbc9e1d4ecf98ca81
waplet <waplet@gmail.com>
2021-11-09 20:39:22 +0200

    Fix category tree in untranslated language

--- resources/views/categories/partials/tree.blade.php
+++ resources/views/categories/partials/tree.blade.php
@@ -1,4 +1,8 @@
 @unless (empty($categories))
+    @php
+        $locale = LaravelLocalization::getCurrentLocale();
+    @endphp
+
     <ul class="category-tree">
         @foreach ($categories as $category)
             <li data-jstree='{"icon":"glyphicon glyphicon-{{ $category->type == 'system' ? 'lock' : $category->inputTypeIcon() }}", "selected":"{!! (isset($selected) && in_array($category->id, $selected)) !!}", "opened":"{!! (isset($selected) && in_array($category->id, $selected)) !!}", "cat":"{{$category->id}}" @if(isset($edit_checklist) && $edit_checklist == true), "disabled":"{{ $category->required ? true : false }}" @endif }'>
@@ -6,15 +10,10 @@
                 @if(isset($edit_taxonomy) && $edit_taxonomy == true)
                 <a href="{{ route('categories.show', [ $category->getKey() ]) }}">
                 @endif
-                    
-                    @php
-                        $locale = LaravelLocalization::getCurrentLocale();
-                    @endphp
-
                     @if(isset($category->trans[$locale]))
                     {{ $category->trans[$locale] }}
                     @else
-                    {{ $category->name or '' }}
+                    {{ $category->name ?? ''}}
                     @endif
 
                     @if($category->required)
